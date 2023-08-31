191a3d100d9df7426b8e93c26bc7ace89e90c761
Safianu <safianusani2004@gmail.com>
2023-08-31 18:29:24 +0100

    the eye

--- app/src/main/java/com/safianu/theeye/MainActivity.java
+++ app/src/main/java/com/safianu/theeye/MainActivity.java
@@ -2,7 +2,9 @@ package com.safianu.theeye;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+            window.setNavigationBarColor(this.getResources().getColor(R.color.black));
+        }
+
     }
 }
